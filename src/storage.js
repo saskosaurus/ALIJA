b04795c0b6639cf9f16f6bb3e27294af445cab29
saskosaurus <52588396+saskosaurus@users.noreply.github.com>
2020-12-18 17:20:19 +0100

    Add files via upload

---
+++ src/storage.js
@@ -0,0 +1,38 @@
+export default{
+    
+    rijeci: [
+        { ime: 'mikroekonomija', bodovi: 5 },
+        { ime: 'azija', bodovi: 5 },
+        { ime: 'rima', bodovi: 5 },
+        { ime: 'stihovi', bodovi: 5 },
+        { ime: 'fora', bodovi: 5 },
+        { ime: 'slap', bodovi: 5 }, 
+      ],
+
+
+     
+      
+
+      teamOne:{
+        igraci:[],
+        bodovi:0
+      },
+      teamTwo:{
+        igraci:[],
+        bodovi:0
+      },
+      teamThree:{
+        igraci:[],
+        bodovi:0
+      },
+      teamFour:{
+        igraci:[],
+        bodovi:0
+      },
+      teamFive:{
+        igraci:[],
+        bodovi:0
+      },
+    
+    
+    }
